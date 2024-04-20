6e7bd4cabed12f2f9321a9b094e73df3db5ea83d
agro40tech <volkovvova67@gmail.com>
2024-04-20 15:18:12 +0400

    feat: базовый функционал сайдбара с параметрами

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,38 @@
+import { clsx } from 'clsx';
+
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { forwardRef } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type TProps = {
+	onClick: OnClick;
+	open: boolean;
+};
+
+export const ArrowButton = forwardRef<HTMLDivElement, TProps>((props, ref) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, {
+				[styles.container_open]: props.open,
+			})}
+			onClick={props.onClick}
+			ref={ref}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, { [styles.arrow_open]: props.open })}
+			/>
 		</div>
 	);
-};
+});
+
+// Явно уазываем имя компонента
+ArrowButton.displayName = 'ArrowButton';
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,155 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
+import { SyntheticEvent, useLayoutEffect, useRef, useState } from 'react';
 
+import { clsx } from 'clsx';
+
+import { Button } from 'components/button';
+import { ArrowButton } from 'components/arrow-button';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+
+import { Text } from '../text';
+import { Select } from '../select';
+import { Separator } from '../separator';
+import { RadioGroup } from '../radio-group';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type TProps = {
+	setState: (value: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ setState }: TProps) => {
+	const [open, setOpen] = useState<boolean>(false);
+
+	// Состояние параметров формы
+	const [selectedOpt, setSelectedOpt] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// Refs для элементов
+	const sidebarRef = useRef<HTMLElement | null>(null);
+	const rowBtnRef = useRef<HTMLDivElement | null>(null);
+
+	// Хендлеры
+	const handleSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+		setState(selectedOpt);
+	};
+
+	const handleReset = () => {
+		setState(defaultArticleState);
+		setSelectedOpt(defaultArticleState);
+	};
+
+	const handleClickOutside = (e: MouseEvent) => {
+		if (
+			!rowBtnRef.current?.contains(e.target as Node) &&
+			!sidebarRef.current?.contains(e.target as Node)
+		) {
+			setOpen(false);
+		}
+	};
+
+	useLayoutEffect(() => {
+		if (open) {
+			document.addEventListener('click', handleClickOutside);
+		} else {
+			document.removeEventListener('click', handleClickOutside);
+		}
+
+		// В случае если в дальнейшем его будут демонтировать то же убираем слушатель
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	}, [open]);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={() => setOpen(!open)} open={open} ref={rowBtnRef} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: open })}
+				ref={sidebarRef}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text as='h1' size={31} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
+
+					{/* Выбор шрифта */}
+					<Select
+						onChange={(option) =>
+							setSelectedOpt({
+								...selectedOpt,
+								fontFamilyOption: option,
+							})
+						}
+						options={fontFamilyOptions}
+						selected={selectedOpt.fontFamilyOption}
+						title='шрифт'
+					/>
+
+					{/* Выбор размеров шрифта */}
+					<RadioGroup
+						name='font'
+						onChange={(option) =>
+							setSelectedOpt({
+								...selectedOpt,
+								fontSizeOption: option,
+							})
+						}
+						options={fontSizeOptions}
+						selected={selectedOpt.fontSizeOption}
+						title='размер шрифта'
+					/>
+
+					{/* Выбор цвета шрифта */}
+					<Select
+						onChange={(option) =>
+							setSelectedOpt({
+								...selectedOpt,
+								fontColor: option,
+							})
+						}
+						options={fontColors}
+						selected={selectedOpt.fontColor}
+						title='цвет шрифта'
+					/>
+
+					{/* Разделение формы */}
+					<Separator />
+
+					{/* Выбор цвета страницы */}
+					<Select
+						onChange={(option) =>
+							setSelectedOpt({
+								...selectedOpt,
+								backgroundColor: option,
+							})
+						}
+						options={backgroundColors}
+						selected={selectedOpt.backgroundColor}
+						title='цвет фона'
+					/>
+
+					{/* Выбор ширины контента */}
+					<Select
+						onChange={(option) =>
+							setSelectedOpt({
+								...selectedOpt,
+								contentWidth: option,
+							})
+						}
+						options={contentWidthArr}
+						selected={selectedOpt.contentWidth}
+						title='ширина контента'
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={handleReset} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
